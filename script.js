b3bff9ae4060321a2da8918fe73688c51528fd6f
Karthikeyan Palaniswamy <karthikeyan.forethought@gmail.com>
2020-03-28 14:06:00 +0100

    funtional stock listing app

---
+++ script.js
@@ -0,0 +1,95 @@
+(function(){
+
+    const data = [
+        { category : "Sporting Goods", price : "$49.99", stocked : true, name : "Football" },
+        { category : "Sporting Goods", price : "$9.99", stocked : true, name : "Baseball" },
+        { category : "Sporting Goods", price : "$29.99", stocked : false, name : "Basketball" },
+        { category : "Electronics", price : "$99.99", stocked : true, name : "iPod Touch" },
+        { category : "Electronics", price : "$399.99", stocked : false, name : "iPhone 5" },
+        { category : "Electronics", price : "$199.99", stocked : true, name : "Nexus 7" }
+    ];
+
+    const $stockElement = document.getElementById('showStockedItem');
+    const $searchInput = document.getElementById('searchProducts');
+
+    const listCategoriesFromData = (data)=>{
+
+        let categoryList = [];
+        for (let row of data) {
+            if (!categoryList.includes(row['category'])) {
+                categoryList.push(row['category']);
+            }
+        }
+
+        return categoryList;
+    };
+
+
+    const getItemsInStock = (data)=>{
+
+        let itemsInStock = [];
+        for (let row of data) {
+            if (row['stocked']) {
+                itemsInStock.push(row);
+            }
+        }
+
+        return itemsInStock;
+    };
+
+    const getItemsFromCategory = (data, category, inStock)=>{
+
+        let itemsInStock = inStock ? getItemsInStock(data) : data;
+        let items = [];
+        for (let row of itemsInStock) {
+            if (row['category'] === category) {
+                items.push(row);
+            }
+        }
+
+        return items;
+    };
+
+
+    const buildHTMLStructureOfItems = (data, inStock = false)=>{
+
+        let htmlStructure = ``;
+        for (let i of listCategoriesFromData(data)) {
+            htmlStructure += `<tr><td colspan="2">${i}</td></tr>`;
+            for (let j of getItemsFromCategory(data, i, inStock)) {
+                htmlStructure += `<tr><td>${j['name']}</td><td>${j['price']}</td></tr>`;
+            }
+        }
+
+        return htmlStructure;
+    };
+
+    function appendToTableBody(tbodyData){
+        document.getElementsByTagName('tbody')[0].innerHTML = tbodyData;
+    }
+
+    appendToTableBody(buildHTMLStructureOfItems(data));
+
+    $stockElement.addEventListener('change', function(){
+
+        if (this.checked) {
+            appendToTableBody(buildHTMLStructureOfItems(data, true));
+            return;
+        }
+        appendToTableBody(buildHTMLStructureOfItems(data));
+    });
+
+    $searchInput.addEventListener('input', function(){
+
+        let filterData = [];
+        for (let row of data) {
+            if (row['name'].toLowerCase().indexOf(this.value.toLowerCase()) > -1) {
+                filterData.push(row);
+            }
+
+        }
+        appendToTableBody(buildHTMLStructureOfItems(filterData, $stockElement.checked));
+        // return filterData;
+    });
+
+})();
